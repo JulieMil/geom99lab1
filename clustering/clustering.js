dae779a91c352d7cbab6f461279aa84c189d4261
JulieMil <146376040+JulieMil@users.noreply.github.com>
2024-01-16 15:22:39 -0500

    Updating center

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -1,7 +1,7 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 3,
-    center: { lat: -28.024, lng: 140.887 },
+    center: { lat: 48.750909, lng: -84.101772},
   });
   // Create an array of alphabetical characters used to label the markers.
   const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
